5725021bb2af454de3f963cc25269bb95fe3cbde
Adam Goucher <adam@goucher.ca>
2012-06-29 14:09:15 -0400

    1.0.5 to get PHPHARchive

--- examples/ebay/scripts/BlacklistTest.php
+++ examples/ebay/scripts/BlacklistTest.php
@@ -3,6 +3,7 @@ namespace WebDriver;
 
 require_once 'tailored/EBayTestCase.php';
 require_once 'pages/ShirtPage.php';
+require_once 'PHPHARchive/HAR.php';
 
 class BlacklistTest extends EBayTestCase {
     /**
@@ -12,13 +13,24 @@ class BlacklistTest extends EBayTestCase {
     * @group blacklist
     */
     public function collar_style() {
-        $this->client->blacklist("http://www\\.facebook\\.com/.*", 200);
-        $this->client->blacklist("http://static\\.ak\\.fbcdn\\.com/.*", 200);
+        $this->client->blacklist("http://www\\.facebook\\.com/.*", 306);
+        $this->client->blacklist("http://static\\.ak\\.fbcdn\\.com/.*", 306);
         $sp = new ShirtPage($this->session);
+        $this->client->new_har("shirts");
         $sp->go_to_mens_dress_shirts();
-        $sp->change_collar_style("Banded (Collarless)");
-        $this->assertTrue($sp->is_collar_selected("Banded (Collarless)"));
-        sleep(15);
+        $har = $this->client->har;
+        
+        var_dump($har);
+
+        $h = new \PHPHARchive_HAR($har);
+        $entries = $h->get_entries_by_page_ref("page_1_0");
+        $three_oh_sixes = array();
+        foreach ($entries as $entry) {
+          if ($entry->response->status == 306) {
+            array_push($three_oh_sixes, $entry);
+          }
+        }
+        $this->assertEquals(count($three_oh_sixes), 1);
     }
 
 }
